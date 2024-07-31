6ca761b3fa66d500fa17e6566350853520213b24
sayutef <sayueszucha@gmail.com>
2024-07-30 22:05:24 -0600

    first commit

---
+++ src/controller/script.mjs
@@ -0,0 +1,33 @@
+
+
+let filaSize = document.getElementById("fila-ipt");
+let columnSize = document.getElementById("columna-ipt");
+let createBtn = document.getElementById("btn-crear");
+let stuffedSearch = document.getElementById("number-ipt");
+let stuffedSearchBtn = document.getElementById("btn-rellenar");
+let sumSearch = document.getElementById("suma-ipt");
+let sumSearchBtn = document.getElementById("btn-search");
+let burble = document.getElementById("burble-btn")
+let matriz = new Matriz();
+
+createBtn.addEventListener("click", ()=>{
+    let fila = parseInt(filaSize.value);
+    let columna = parseInt(columnSize.value);
+
+    matriz.crearMatriz(fila, columna);
+    console.table(matriz.getMatrix());
+})
+
+stuffedSearchBtn.addEventListener("click", ()=>{
+    let num = parseInt(stuffedSearch.value);
+    matriz.rellenarMatriz(num);
+
+    console.table(matriz.getMatrix());
+})
+
+burble.addEventListener("click", ()=>{
+    matriz.burble();
+    console.table(matriz.getMatrix());
+})
+
+
